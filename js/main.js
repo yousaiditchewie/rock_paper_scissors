44141d6b67c05c09c1d045b54d970817218ddfea
Jacob Allen <jedijake@me.com>
2017-05-17 21:19:24 -0700

    link js and css to html

---
+++ js/main.js
@@ -0,0 +1,33 @@
+console.log(`Rock Paper Scissors Loaded!`)
+
+// **** MODEL: DATA MODEL ****
+
+var gameIsWon  = false;
+var gameLength = 1;
+var player1    = 'Player 1';
+var player2    = 'Player 2';
+
+var startGame = function
+
+// **** MODEL: GAME LOGIC ****
+
+// WINNING PLAYS
+
+var evaluateWinner = function(player1Play, player2Play) {
+  tie = false;
+  if (player1Play === player2Play) {
+    return tieGame();
+  } else if (player1Play === 1 && player2Play === 2 || player1Play === 2 && player2Play === 3 || player1Play === 3 && player2Play === 1) {
+    return winner = player2;
+  } else {
+    return winner = player1;
+  }
+}
+var tieGame = function(tie) {
+  return tie = true;
+}
+
+
+// **** GLOBAL VARIABLES ****
+var winner,
+    tie;
