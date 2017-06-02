92b80d10e43f40c1e37c0df75adfa94b85085a3c
Jacob Allen <jedijake@me.com>
2017-06-01 21:15:30 -0700

    add game board to dom

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ var game = {
 }
 
 function startGame(){
-  game.isWon    = false;
-  game.length   = 1;
+  game.isWon        = false;
+  game.length       = 1;
   game.player1      = 'Player 1';
   game.player1Score = 0;
   game.player2      = 'Player 2';
@@ -23,12 +23,14 @@ function evaluateWinner(player1Play, player2Play) {
   if (player1Play === player2Play) {
     return tieGame();
   } else if (player1Play === 1 && player2Play === 2 || player1Play === 2 && player2Play === 3 || player1Play === 3 && player2Play === 1) {
-    return winner = player2;
+    game.player2Score++;
+    return winner = game.player2;
   } else {
-    return winner = player1;
+    game.player1Score++;
+    return winner = game.player1;
   }
 }
-function tieGame(tie) {
+function tieGame() {
   return tie = true;
 }
 function generateComputerPlay() {
