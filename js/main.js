f8934a64fcc5f6758c9602d75598cd0022cfde88
Jacob Allen <jedijake@me.com>
2017-06-07 23:25:57 -0700

    write render function that fills html and scr's based on the game state object

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('readystatechange', function(evt) {
-  console.log(evt.path[0].readyState);
   if (evt.path[0].readyState === 'complete') {
     console.log(`Rock Paper Scissors Loaded!`);
+    startGame();
   }
 });
 
@@ -13,38 +13,44 @@ var messages = [
   'You lose...'
 ];
 var imageUrls = [
+  './assets/computer.png',
   './assets/rock.png',
   './assets/paper.png',
-  './assets/scisors.png',
-  './assets/computer.png'
+  './assets/scissors.png'
 ]
 var game = {
-  isWon:        false,
-  length:       1,
-  player1:      'Player 1',
-  player2:      'Computer',
-  player1Score: 0,
-  player2Score: 0,
-  message:      messages[0]
+  isWon:          false,
+  length:         1,
+  player1:        'Player 1',
+  player2:        'Computer',
+  player1Score:   0,
+  player2Score:   0,
+  message:        messages[0],
+  computerImgUrl: imageUrls[0],
+  tie:            false
 }
-
+var rock     = 1;
+var paper    = 2;
+var scissors = 3;
 function startGame(){
-  game.isWon        = false;
-  game.length       = 1;
-  game.player1      = 'Player 1';
-  game.player2      = 'Player 2';
-  game.player1Score = 0;
-  game.player2Score = 0;
-  game.message      = messages[0];
+  game.isWon          = false;
+  game.length         = 1;
+  game.player1        = 'Player 1';
+  game.player2        = 'Player 2';
+  game.player1Score   = 0;
+  game.player2Score   = 0;
+  game.message        = messages[0];
+  game.computerImgUrl = imageUrls[0];
+  game.tie            = false;
+  render();
 };
 
 
 // **** MODEL: GAME LOGIC ****
 function evaluateWinner(player1Play, player2Play) {
-  tie = false;
   if (player1Play === player2Play) {
     return tieGame();
-  } else if (player1Play === 1 && player2Play === 2 || player1Play === 2 && player2Play === 3 || player1Play === 3 && player2Play === 1) {
+  } else if (player1Play === rock && player2Play === paper || player1Play === paper && player2Play === scissors || player1Play === scissors && player2Play === rock) {
     game.player2Score++;
     return winner = game.player2;
   } else {
@@ -53,7 +59,7 @@ function evaluateWinner(player1Play, player2Play) {
   }
 }
 function tieGame() {
-  return tie = true;
+  return game.tie = true;
 }
 function generateComputerPlay() {
   return player2Play = Math.floor(Math.random() * (4 - 1) + 1);
@@ -71,8 +77,16 @@ function checkGameIsOver() {
 
 // **** DOM ELEMENT VARIABLES ****
 var messageBoard = document.getElementById('message-board');
+var computerImg  = document.getElementById('computer-img');
+var player1Score = document.getElementById('player-1-score');
+var player2Score = document.getElementById('player-2-score');
 // **** RENDER FUNCTIONS ****
-
+function render() {
+  messageBoard.innerHTML = game.message;
+  computerImg.src = game.computerImgUrl;
+  player1Score.innerHTML = game.player1Score;
+  player2Score.innerHTML = game.player2Score;
+}
 
 // **** GLOBAL VARIABLES ****
 var winner,
