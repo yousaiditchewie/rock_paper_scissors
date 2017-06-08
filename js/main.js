6a03282aad8d04fd78631b5a72e012497619e982
Jacob Allen <jedijake@me.com>
2017-06-07 22:51:18 -0700

    revise game data model to include messages, image url's, and an event listener on the document that checks its ready state

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
-console.log(`Rock Paper Scissors Loaded!`)
+document.addEventListener('readystatechange', function(evt) {
+  console.log(evt.path[0].readyState);
+  if (evt.path[0].readyState === 'complete') {
+    console.log(`Rock Paper Scissors Loaded!`);
+  }
+});
 
 // **** MODEL: DATA MODEL ****
+var messages = [
+  'Choose your play...',
+  'Tie game. Play again...',
+  'You Win!!!',
+  'You lose...'
+];
+var imageUrls = [
+  './assets/rock.png',
+  './assets/paper.png',
+  './assets/scisors.png',
+  './assets/computer.png'
+]
 var game = {
-  isWon: false,
-  length: 1,
+  isWon:        false,
+  length:       1,
+  player1:      'Player 1',
+  player2:      'Computer',
   player1Score: 0,
-  player2Score: 0
+  player2Score: 0,
+  message:      messages[0]
 }
 
 function startGame(){
   game.isWon        = false;
   game.length       = 1;
   game.player1      = 'Player 1';
-  game.player1Score = 0;
   game.player2      = 'Player 2';
+  game.player1Score = 0;
   game.player2Score = 0;
+  game.message      = messages[0];
 };
+
 
 // **** MODEL: GAME LOGIC ****
 function evaluateWinner(player1Play, player2Play) {
@@ -46,6 +68,10 @@ function checkGameIsOver() {
     return game.isWon = false;
   }
 }
+
+// **** DOM ELEMENT VARIABLES ****
+var messageBoard = document.getElementById('message-board');
+// **** RENDER FUNCTIONS ****
 
 
 // **** GLOBAL VARIABLES ****
